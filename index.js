ecae202031b804b464be48e89452bcd3b06d16b6
myrionsc <marpht23@gmail.com>
2015-04-21 18:39:22 +0200

    removed comments

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var app = express();
 var Parse = require('parse').Parse;
 var bodyParser = require('body-parser');
 
+// keys for sdm-testdb on parse
 Parse.initialize("tddpZ4wQt9lFdynC5u5WcjU9RG2HunQl5epfPZKp", "rtWeegY0PM1YMKGOdFNcP9F1d8ri2zNJvfUn7Rht");
-// keys for sdm-testdb
 
 var SDM_Current_Input = Parse.Object.extend("SDM_Current_Input");
 
@@ -27,27 +27,7 @@ app.all('*',function(req,res,next)
     next();
 });
 
-//app.get('/', function(request, response) {
-//    console.log("i have been kicked");
-//
-//
-//    //var peopleJson = "{" +
-//    //    "People: [" +
-//    //        "{" +
-//    //            "firstname: 'Thomas'," +
-//    //            "lastname: 'Raith'" +
-//    //        "}," +
-//    //        "{" +
-//    //            "firstname: 'Harry'," +
-//    //            "lastname: 'Dresden'" +
-//    //        "}" +
-//    //    "]" +
-//    //"}";
-//    //response.type('json');
-//    //response.send(peopleJson);
-//    response.send("fuck off");
-//});
-
+// get
 app.get('/get/latestinput', function (request, response) {
     console.log("im trying le get latest input");
 
@@ -94,7 +74,7 @@ app.get('/get/history', function (request, response) { // params: skip, limit
 
 
 
-
+//post
 app.post('/post/newinput', function (request, res) {
     var newtext = new SDM_Current_Input();
     var Text = request.body.text;
@@ -109,99 +89,3 @@ app.post('/post/newinput', function (request, res) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
-//
-//
-//
-
-//var express = require('express');
-//var app = express();
-////var bodyParser = require('body-parser');
-////var mongodb = require('mongodb');
-////var mongouri = process.env.MONGOLAB_URI;
-//var Parse = require('parse').Parse;
-//
-//Parse.initialize("tddpZ4wQt9lFdynC5u5WcjU9RG2HunQl5epfPZKp", "rtWeegY0PM1YMKGOdFNcP9F1d8ri2zNJvfUn7Rht");
-//
-//app.set('port', (process.env.PORT || 5000));
-//app.use(express.static(__dirname + '/public'));
-////app.use(bodyParser.json());
-////app.use(bodyParser.urlencoded());
-//
-//
-//
-//var TextObj = Parse.Object.extend("Text");
-//var SDM_Current_Input = Parse.Object.extend("SDM_Current_Input");
-//
-////app.all('*',function(req,res,next)
-////{
-////    if (!req.get('Origin')) return next();
-////
-////    res.set('Access-Control-Allow-Origin','*');
-////    res.set('Access-Control-Allow-Methods','GET,POST');
-////    res.set('Access-Control-Allow-Headers','X-Requested-With,Content-Type');
-////
-////    if ('OPTIONS' == req.method) return res.send(200);
-////
-////    next();
-////});
-//
-//app.get('/', function(request, response) {
-//
-//
-//    //var result = ;
-//    //var times = process.env.TIMES ||;
-//    //for (i=0; i < times; i)
-//    //    result += cool;
-//    //response.send(result);
-//});
-//
-//// get
-////app.get('/get/TextArray', function (request, response) {
-////    var query = new Parse.Query(TextObj);
-////    query.find({
-////        success: function(text) {
-////            var textarray = [];
-////            for (var i = 0; i < text.length; ++i) {
-////                textarray.push(text[i].get('text'));
-////            }
-////            response.type('json');
-////            response.send(textarray);
-////        }
-////    });
-////});
-//app.get('/SDM/get/CurrentInput', function (request, response) {
-//    console.log("im trying le get");
-//
-//    var query = new Parse.Query(SDM_Current_Input);
-//    query.limit(1);
-//    query.descending("createdAt");
-//    query.first({
-//        success: function(ParseObj) {
-//            response.type('json');
-//            response.send(ParseObj.get("Input"));
-//        },
-//        error: function() {
-//            response.send("error");
-//        }
-//    });
-//});
-//
-//// post
-////app.post('/post/TextPost', function (request, res) {
-////    var newtext = new TextObj();
-////    Text = request.body;
-////    newtext.save({text: Text}).then(function(object) {
-////        res.send("Success");
-////    });
-////});
-////app.post('/SDM/post/InputString', function (request, res) {
-////    var newtext = new SDM_Current_Input();
-////    Text = request.body.text;
-////    newtext.save({Input: Text}).then(function(object) {
-////        res.send("Success");
-////    });
-////});
-//
-//app.listen(app.get('port'), function() {
-//    console.log("Node app is running at localhost:" + app.get('port'));
-//});
