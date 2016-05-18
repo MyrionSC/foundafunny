d602e9fd68034b702d401291ba870ffcd379de85
myrionsc <marpht23@gmail.com>
2016-05-18 18:21:02 +0000

    documentation text added

--- pages/Scripts/Controllers/DocumentationCtrl.js
+++ pages/Scripts/Controllers/DocumentationCtrl.js
@@ -1,3 +1,6 @@
-app.controller('DocumentationCtrl', function($scope) {
-    
+app.controller('DocumentationCtrl', function($scope, $document) {
+    var s = $scope;
+
+    var h = $document[0].body.clientHeight - 150;
+    s.height = h + "px";
 });
